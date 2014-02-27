32cee2ef248e1f544ac833f227f054ad4e5edc53
Huy Nguyen <nguyt330@newschool.edu>
2014-02-27 17:43:57 -0500

    no message

---
+++ paint_test/painttest.csproj/Assets/spheremove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spheremove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Transform> ().position += new Vector3 (0f, 1f, 0f);
+	}
+}
